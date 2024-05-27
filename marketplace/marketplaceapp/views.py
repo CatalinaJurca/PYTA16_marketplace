e71b93b235e85cee9814056cd3e9987b3aa60267
CatalinaJ <tudose.catalina@yahoo.com>
2024-05-27 21:07:44 +0300

    update models

--- marketplace/marketplaceapp/views.py
+++ marketplace/marketplaceapp/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
-
-
+from .models import Product
+from django.shortcuts import get_object_or_404
 def index(request):
     return HttpResponse("Hello")
+
+def product_list(request):
+    #read all product from db
+    all_products=Product.objects.all()
+    print(all_products)
+    return HttpResponse("Product_list")
+
+def product_detail(request, product_id):
+    # product=Product.objects.get(pk=product_id)
+    product=get_object_or_404(Product,pk=product_id)
+    print(product)
+    return HttpResponse(f"Product_detail:{product_id}")
